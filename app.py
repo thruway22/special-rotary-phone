23320921c94dfb1efc0bda708a75ea94524d2c7c
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-21 01:52:42 +0300

    u

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ st.title('BakerTimeSheetGenerator')
 form = st.form('input_form')
 form.text_input('Name', placeholder=None, label_visibility="visible")
 form.number_input('ID')
+form.number_input('Day Rate (SAR)')
 
 submitted = form.form_submit_button('Generate PDF File')
