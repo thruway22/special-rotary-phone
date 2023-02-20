c88d3b4d5570b2add7b28eacdf258be8cd6e3dd0
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-21 02:01:35 +0300

    u

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ date_in = form.date_input('Date In')
 date_out = form.date_input('Date Out')
 wstl_name = form.selectbox('Wellsite Team Leader', ['KL', 'PO', 'SB', 'AM'])
 form.expander('Other settings')
-expander.write(\"\"\"
-    The chart above shows some numbers I picked for you.
-    I rolled actual dice for these, so they're *guaranteed* to
-    be random.
-\"\"\")
+expander.write('ikhy')
 
 submitted = form.form_submit_button('Generate PDF File')
