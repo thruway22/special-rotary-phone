3643d8a4b04ede52e4ee0dc80c9adf89d48b41dc
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-21 01:53:54 +0300

    u

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 st.title('BakerTimeSheetGenerator')
 form = st.form('input_form')
-form.text_input('Name', placeholder=None, label_visibility="visible")
+form.text_input('Name')
 form.number_input('ID')
 form.number_input('Day Rate (SAR)')
+form.text_input('Wellsite Team Leader Name')
 
 submitted = form.form_submit_button('Generate PDF File')
