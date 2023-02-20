6c58e877c2d829c25055a0f5cfdfb3432c1ccffb
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-21 01:50:25 +0300

    u

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 st.title('BakerTimeSheetGenerator')
 form = st.form('input_form')
-from.text_input('Name', placeholder=None, label_visibility="visible")
+form.text_input('Name', placeholder=None, label_visibility="visible")
 form.number_input('ID')
