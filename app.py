6b0f46052051dc7824d2fc85cf3dd4672d90fb38
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-22 02:13:33 +0300

    u

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ if submitted:
         pdf_output = BytesIO()
         #result_file = open(pdf_output, "w+b")
         pisa_stat = pisa.CreatePDF(out_stream.read(), dest=pdf_output)
-        pdf_output.close()
+        #pdf_output.close()
         pdf_output.seek(0)
 
         st.download_button(
