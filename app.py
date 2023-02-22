066a0b9ae26bab991b753adc7ad2d14ebfd7afc6
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-02-22 06:13:01 +0300

    u

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import calendar
 
 st.title('BakerTimeSheetGenerator')
 form = st.form('input_form')
-left, middle, right = form.columns ([3, 2, 1])
+left, middle, right = form.columns ([3, 2, 2])
 employee_name = left.text_input('Name')
 employee_id = middle.number_input('ID', step=1)
 employee_rate = right.number_input('Day Rate (SAR)')
