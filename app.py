5cdf854201d72c22863b18cac0a7f848580f5152
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-03-05 03:01:14 +0300

    u

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from xhtml2pdf import pisa
 from bs4 import BeautifulSoup
 
 st.title('TimesheetGO')
+st.write('An easy and quick timesheet maker for Baker boys ❤️')
 form = st.form('input_form')
 left, middle, right = form.columns ([2, 1, 1])
 employee_name = left.text_input('Name')
