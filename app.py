e3925de261c9b18b930a90c10dafbd333a81e21f
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-03-05 03:05:22 +0300

    u

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from xlsx2html import xlsx2html
 from xhtml2pdf import pisa
 from bs4 import BeautifulSoup
 
-st.title('💰 TimesheetGO')
+st.title('💰 TimesheetMaker')
 st.write('An easy and quick timesheet maker for Baker boys.')
 form = st.form('input_form')
 left, middle, right = form.columns ([2, 1, 1])
