381ac3a92b4a277da0b4643dd1dac8428e61bdc1
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-03-12 19:26:27 +0300

    u

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ if submitted:
             st.write(saved_files)
             st.download_button(
                 label="Download pdf Page",
-                data=saved_files.read(),
+                data=result,
                 file_name="report.pdf",
                 mime="application/octet-stream")
             st.write('Vola!')
